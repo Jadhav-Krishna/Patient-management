fe479e2372c5e42b6b399fc47beea434f3919b20
Krishna Jadhav <krishnajadhav85017@gmail.com>
2024-04-26 08:23:58 +0530

    login part

---
+++ data.js
@@ -0,0 +1,24 @@
+module.exports = [
+    
+    {
+      name: "Loffy",
+      id: 0o111221113,
+      age: 20,
+      gender: "Female",
+      email: "Loffy@gmail.com",
+      address: "Shanti hostel , Bhopal",
+      img: "https://images.unsplash.com/photo-1711894904366-4f6f7e8affea?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZWwlMjBwb3RyYWl0c3xlbnwwfHwwfHx8MA%3D%3D",
+    },
+    
+    {
+      name: "Ace",
+      id: 0o111221177,
+      age: 23,
+      gender: "Female",
+      email: "ace@gmail.com",
+      address: "One piece , Anime",
+      img: "https://images.unsplash.com/photo-1701357156569-65249e5c42fb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTI0fHxtb2RlbCUyMHBvdHJhaXRzfGVufDB8fDB8fHww",
+    },
+    
+  ];
+  
--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 const express = require('express');
 const path = require('path');
+const data = require("./data");
 const fs = require('fs');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.set('view engine', 'ejs');
 
 
 app.get("/",function(req,res){
-    res.render("login")
+    res.render("index")
 })
 
-app.get("/",function(req,res){
-    
+app.get("/login",function(req,res){
+    res.render( "login" )
 })
-app.listen("3000",function(){
-    console.log("hello")
+
+app.get("/doctor",function(req,res){
+    res.render( "doctor" )
+})
+
+app.get("/doctorprofile", (req, res) => {
+    res.render("doctorprofile", { data });
+  });
+
+app.get("/doctorprofile/:userid", (req, res) => {
+    var user = data.find((val) => val.id === Number(req.params.userid));
+    if (user) res.render("user", { user });
+    else res.render("error");
+  });
+
+app.listen("3000",() => {
+    console.log("Server is now live....")
 })
 
 
