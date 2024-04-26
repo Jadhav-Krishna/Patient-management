bd4b48244dee217cb19fefb470831c6a97a36335
Krishna Jadhav <krishnajadhav85017@gmail.com>
2024-04-27 01:14:02 +0530

    fake data done

--- patientdetail.js
+++ patientdetail.js
@@ -1,4 +1,9 @@
+const { name } = require("ejs");
+
 module.exports = [
-    {id:587496123 ,name:"karan raj gupta",bimari:"bawasr",gender:"male"},
-    {id:978465231 ,name:"Sarah Khan",bimari:"mangala",gender:"female"}
+    {photo:"https://i.pinimg.com/564x/05/32/54/053254e3409206516d9d974803858661.jpg",id:898965581 , name:"Mike Smith", gender:"male" ,age:21},  
+    {photo:"https://i.pinimg.com/564x/a6/f5/65/a6f565ab85d0915e7098dffb3bb943c1.jpg",id:123456789 ,name:"karan raj gupta",gender:"male" , age:19},
+    {photo:"https://i.pinimg.com/564x/04/ea/a9/04eaa9c5edd0a25837e26b9188f27bb0.jpg",id:987654321 ,name:"Sarah Khan",gender:"female"  ,age:24},
+    {photo:"https://i.pinimg.com/564x/3c/94/3c/3c943c701dfc9f97d49a8cf22f9b6aee.jpg",id:543216789 ,name:"Adarsh Raj",gender:"male"  ,age:32},
+    {photo:"https://i.pinimg.com/564x/6d/2c/2a/6d2c2a0a033d11e7f902a219b9369664.jpg",id:543212345 ,name:"Nezuko Kamado",gender:"female"  ,age:16},
 ]
